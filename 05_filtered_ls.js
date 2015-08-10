cf38f1c442fd4cc44a133b324931e0c3444c3244
Lianne <lyharrison@sky.com>
2015-08-10 20:18:26 +0000

    exercise 5 complete

--- 05_filtered_ls.js
+++ 05_filtered_ls.js
@@ -1,5 +1,11 @@
 var fs = require('fs');
 
-fs.readdir(process.argv[2], function (err, files) {
-  console.log(files);
+fs.readdir(process.argv[2], function (err, data) {
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].split(".")[1] === process.argv[3]){
+      console.log(data[i]);
+    };
+  };
+
 });
